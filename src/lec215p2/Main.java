357ca694be7e6cc001baa442432e303eb6e53e4a
eugeny <eberkunsky@gmail.com>
2018-06-27 21:30:21 +0300

    work with maps

--- src/lec215p2/Main.java
+++ src/lec215p2/Main.java
@@ -5,6 +5,23 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+//        workWithSets();
+        workWithMaps();
+    }
+
+    private static void workWithMaps() {
+        HashMap<String,Integer> map = new HashMap<>();
+        System.out.println(map.put("one", 1));
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        System.out.println(map.put("four", -2));
+//        map.put(null, null); // НЕ ДЕЛАЙТЕ ТАК НИКОГДА!!!
+        System.out.println(map);
+    }
+
+    private static void workWithSets() {
         Set<Integer> set = new HashSet<>(Arrays.asList(17,1,4,2,7,3,5,6,8,9,16,20));
         System.out.println(set);
         System.out.println(set.size());
